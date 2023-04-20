55967b0069e8621feb7cafedb541aa7692bfeebb
pairofAces <karanchauhan16@gmail.com>
2023-04-20 11:57:01 -0400

    created file for validAnagram

---
+++ Arrays&Hashing/validAnagram.py
@@ -0,0 +1,11 @@
+# LC 242 - Valid Anagram
+
+# Given two strings, s and t, return true if (t) is an anagram of (s), and false 
+# otherwise.
+
+# note: an anagram is a word of phrase formed by rearranging the letters of a
+# different word or phrase, typically using all the original letters exactly once
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
