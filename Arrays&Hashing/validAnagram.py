2c596e1a5e480c873d052ab0f7edfb7a2d3c4de5
pairofAces <karanchauhan16@gmail.com>
2023-04-21 11:27:38 -0400

    created solution and test for validAnagram

--- Arrays&Hashing/validAnagram.py
+++ Arrays&Hashing/validAnagram.py
@@ -6,6 +6,36 @@
 # note: an anagram is a word of phrase formed by rearranging the letters of a
 # different word or phrase, typically using all the original letters exactly once
 
+# Example
+# input: s = "anagram", t = "nagaram"
+# output: true
+
+# input: s = "rat", t = "car"
+# output: false
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # check base scenario if the lengths of both strings are equal
+        if len(s) is not len(t):
+            return False
         
+        # create two dicitonaries for each input string
+        countS, countT = {}, {}
+
+        # iterate over the (s) input string
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        return countS == countT
+
+# Testing
+s = "anagram"
+t = "nagaram"
+
+s1 = "rat"
+t1 = "car"
+
+test = Solution()
+print(test.isAnagram(s, t))
+print(test.isAnagram(s1, t1))
