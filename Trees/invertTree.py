ee5ebbfc3470e6fec050d91a9a0bad429ace7b46
pairofAces <karanchauhan16@gmail.com>
2023-05-07 22:00:40 -0400

    created solution for invertTree

--- Trees/invertTree.py
+++ Trees/invertTree.py
@@ -11,4 +11,16 @@ class TreeNode:
 
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
+        # base case
+        if not root:
+            return None
         
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
