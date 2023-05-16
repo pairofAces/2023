337882d9aeaa748f4959fda9b269ae0ba89a23f8
pairofAces <karanchauhan16@gmail.com>
2023-05-16 19:31:43 -0400

    created Linked List definition

---
+++ LinkedList/removeDuplicates.py
@@ -0,0 +1,21 @@
+# Remove Duplicates From Linked List
+
+# You're given the head of singly linked list, where the nodes are in sorted order.
+# Create a function that returns a modified version of the Linked List that doesn't
+# contain any nodes with duplicate values. 
+
+# The Linked List should be modified in place, meaning there shouldn't be a new
+# list being produced. 
+    # the modified list should still be in sorted order.
+
+# Each LinkedList node has an integer (value) and a (next) node pointing to
+# the next node in the list or to None/null, if it's the tail of the list. 
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def removeDuplicatesFromLinkedList(linkedList):
+    pass
+
